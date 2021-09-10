848847cd8b730a0307bb54e430ef123b20999949
Arai Shota <mail@arsho.dev>
2021-09-11 01:02:06 +0900

    Init

---
+++ .config/fish/config.fish
@@ -0,0 +1,54 @@
+#
+# Homebrew
+#
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/sbin:$PATH"
+
+# Configuring Completions in fish
+# https://docs.brew.sh/Shell-Completion#configuring-completions-in-fish
+if test -d (brew --prefix)"/share/fish/completions"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+end
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
+#
+# Direnv
+#
+eval (direnv hook fish)
+
+#
+# ASDF
+#
+source /opt/homebrew/opt/asdf/asdf.fish
+
+#
+# Settings
+#
+set -U FZF_LEGACY_KEYBINDINGS 0
+set -x GPG_TTY `tty`
+
+#
+# Alias
+#
+alias f  "fzf"
+alias t  "tmux attach || tmux"
+alias tl "tmux list-sessions"
+
+alias b  "bundle"
+alias bx "bundle exec"
+alias rc "bin/rails console"
+
+alias proco "bundle exec procodile start --dev"
+
+alias gp  "git pull --prune"
+alias gb  "git branch"
+alias gba "git branch -a"
+alias gc  "git commit"
+alias gca "git commit -a"
+alias gd  "git diff --color | diff-so-fancy"
+alias gst "git status -sb"
+alias gl  "git log --pretty=format:'%C(red)%h%Creset %C(green)%cd%Creset %s %C(yellow)[%an] <%ae>%Creset %C(cyan)%d%Creset' --abbrev-commit --date=short"
+alias glm "git log --pretty=format:'%C(red)%h%Creset %C(green)%cd%Creset %s %C(yellow)[%an] <%ae>%Creset %C(cyan)%d%Creset' --abbrev-commit --date=short --no-merges"
+alias grb "git branch --merged | egrep -v '(^\*|master)' | xargs git branch -d"
---
+++ README.md
@@ -0,0 +1,9 @@
+# Dotfiles
+
+```bash
+# Install homebrew
+$ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install tools
+$ brew install asdf diff-so-fancy direnv fish fzf ghq tmux
+```
