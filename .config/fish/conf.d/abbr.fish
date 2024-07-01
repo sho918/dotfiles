b8ad17f63cacd59e21a4637ba9c0525ba94de078
Arai Shota <me@arsho.dev>
2024-07-02 07:43:30 +0900

    Update abbr.fish

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,6 +1,7 @@
 # https://fishshell.com/docs/current/cmds/abbr.html
 abbr -a -g cd z
+abbr -a -g ls eza --icons
 abbr -a -g vim nvim
 abbr -a -g n nvim
 abbr -a -g pp pnpm
-abbr -a -g c zed
+abbr -a -g code zed
