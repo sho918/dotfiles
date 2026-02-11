f072fc82a27d80d110cf4fc15d2be1404b8b95a1
Arai Shota <me@arsho.dev>
2026-02-11 22:12:22 +0900

    update .config/nvim/lua/plugins/asciidoc.lua

--- .config/nvim/lua/plugins/asciidoc.lua
+++ .config/nvim/lua/plugins/asciidoc.lua
@@ -24,6 +24,7 @@ return {
           vim.opt_local.wrap = true
           vim.opt_local.linebreak = true
           vim.opt_local.spell = true
+          vim.opt_local.spelllang:append "cjk"
         end,
       })
     end,
